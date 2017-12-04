857dbec009e76270f7fde9e4aecebd62f197d226
Seth Puckett <puckett.seth@gmail.com>
2017-12-04 15:08:20 -0500

    Updated demo

--- modules/HomeModule.cs
+++ modules/HomeModule.cs
@@ -2,12 +2,16 @@ using Nancy;
 
 namespace nancy_demo.modules
 {
+  public class User
+  {}
+
   public class HomeModule : NancyModule
   {
     public HomeModule()
     {
-      Get("/", _ => "Hello World!!!");
-      Get("/about", _ => "Brought to you buy NancyFX");
+
+      Get("/about", _ => "Hello!");
+
     }
   }
 }
